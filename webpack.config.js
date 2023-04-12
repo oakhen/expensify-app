8097de54e1e06abff93c410e8c17514d802a35c4
akash kumar verma <akkiverma04@gmail.com>
2023-04-13 00:24:06 +0530

    added express server

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,40 @@
 const path = require("path");
-module.exports = {
-  entry: "/src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
+module.exports = (env) => {
+  const isProduction = env === "production"
+  console.log(env)
+  return {
+    entry: "/src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
     },
-    compress: true,
-    port: 9000,
-  },
-  devtool: "eval-cheap-module-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/react", "@babel/env"],
-            // plugins: ["@babel/plugin-proposal-class-properties"],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "public"),
+      },
+      compress: true,
+      port: 9000,
+    },
+    devtool: isProduction ? "source-map" : "eval-cheap-module-source-map",
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/react", "@babel/env"],
+              // plugins: ["@babel/plugin-proposal-class-properties"],
+            },
           },
         },
-      },
-      {
-        test: /\.s?css$/,
-        exclude: /node_module/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-};
+        {
+          test: /\.s?css$/,
+          exclude: /node_module/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
+    },
+  }
+}
